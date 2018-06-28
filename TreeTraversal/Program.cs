87eedf9c57b8185a805f9178095341de4afc10fa
Julian Schacher <jspp@posteo.net>
2018-06-28 16:51:23 +0200

    Provide explanation for commented lines of code.

--- TreeTraversal/Program.cs
+++ TreeTraversal/Program.cs
@@ -18,6 +18,7 @@ namespace TreeTraversal
             Console.WriteLine("DFSRecursivePostorder");
             tree.DFSRecursivePostorder();
 
+            // Uncommenting the following 2 lines will result in an exception thrown because at least one Node of the Tree has more than 2 children and therefor a DFSRecursiveInorderBinary doesn't work.
             // Console.WriteLine("DFSRecursiveInorder (fail)");
             // tree.DFSRecursiveInorderBinary();
             tree = new Tree(3, 2);
