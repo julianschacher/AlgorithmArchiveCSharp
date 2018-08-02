93bbfe08505c3f65ed0afe253502e355ab3fd39e
Julian Schacher <jspp@posteo.net>
2018-08-02 23:02:03 +0200

    Use commas instead of points to make 10,000,000 more readable.

--- MonteCarloIntegration/Program.cs
+++ MonteCarloIntegration/Program.cs
@@ -7,7 +7,7 @@ namespace MonteCarloIntegration
         static void Main(string[] args)
         {
             var monteCarlo = new MonteCarlo();
-            System.Console.WriteLine("Running with 10.000.000 samples.");
+            System.Console.WriteLine("Running with 10,000,000 samples.");
             var piEstimate = monteCarlo.Run(10000000);
             System.Console.WriteLine($"The estimate of pi is: {piEstimate.Estimate}");
             System.Console.WriteLine($"The error is: {piEstimate.Error}");
